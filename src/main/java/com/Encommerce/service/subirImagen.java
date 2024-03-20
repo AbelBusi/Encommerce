9a0cd51ff76882472d4b1159c0f293126b299724
AbelBusi <cesarabelbustamante@gmail.com>
2024-03-19 21:58:36 -0500

    Guardando la funcion que servira para subir imagenes o eliminarlas de un producto

---
+++ src/main/java/com/Encommerce/service/subirImagen.java
@@ -0,0 +1,25 @@
+package com.Encommerce.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author cesar
+ */
+@Service
+public class subirImagen {
+    
+    private String folder="images//";
+    
+    public String guardarImagen(MultipartFile file){
+    
+        if(!file.isEmpty()){
+            
+            byte [] 
+        
+        }
+        return "";
+    }
+    
+}
