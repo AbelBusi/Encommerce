6c19172e6b5536df68d377683f4f912fafa15c3e
AbelBusi <cesarabelbustamante@gmail.com>
2024-03-25 17:57:05 -0500

    Carajo me demoro casi 5 horas arreglando un problema y solo era un / mal puesto xdddd, se implemento la opcion para ver la caracteristica de un producto

--- src/main/java/com/Encommerce/controlador/HomeController.java
+++ src/main/java/com/Encommerce/controlador/HomeController.java
@@ -1,10 +1,15 @@
 package com.Encommerce.controlador;
 
+import com.Encommerce.logica.Producto;
 import com.Encommerce.service.ProductoService;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -18,12 +23,26 @@ public class HomeController {
     @Autowired
     private ProductoService productoService;
 
+    private final Logger loggger = LoggerFactory.getLogger(ProductoController.class);
+
     @GetMapping("/")
     public String home(Model producto) {
 
         producto.addAttribute("productos", productoService.mostrarProductos());
 
         return "administrador/usuario/homeUsuario.html";
+    }
+
+    @GetMapping("verProducto/{id}")
+    public String verProducto(@PathVariable Integer id, Model verProducto) {
+
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+        loggger.info("Producto buscado {}", producto);
+        verProducto.addAttribute("producto", producto);
+        
+        return "administrador/usuario/productoHome.html";
     }
 
 }
