727313f6eb65f681355862bbc8353277c35f1446
AbelBusi <cesarabelbustamante@gmail.com>
2024-03-26 10:51:20 -0500

    se soluciono un error, solo era problema de sintaxis .c

--- src/main/java/com/Encommerce/controlador/HomeController.java
+++ src/main/java/com/Encommerce/controlador/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -44,5 +45,12 @@ public class HomeController {
         
         return "administrador/usuario/productoHome.html";
     }
+    
+    @PostMapping("/carrito")
+    public String addCarrito(){
+    
+        return "administrador/usuario/carrito.html";
+    }
+    
 
 }
